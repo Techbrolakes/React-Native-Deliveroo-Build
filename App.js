0de3109f724fb73f6d671ba5f4947a5018c30aa2
Lekan Dar <Lekandar11@gmail.com>
2022-07-07 16:54:37 -1200

    Setting up react-native-tailwind

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { TailwindProvider } from "tailwindcss-react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your ap</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TailwindProvider>
+      <View style={styles.container}>
+        <Text className="text-red-300">
+          Open up App.js to start working on your ap
+        </Text>
+        <StatusBar style="auto" />
+      </View>
+    </TailwindProvider>
   );
 }
 
